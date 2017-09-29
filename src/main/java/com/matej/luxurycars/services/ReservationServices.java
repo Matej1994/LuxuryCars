c9ef2b3945ff49739954450023d8ce4826cea5fb
Matej1994 <mateusz.maj@pollub.edu.pl>
2017-09-29 10:32:15 +0200

    Final2

--- src/main/java/com/matej/luxurycars/services/ReservationServices.java
+++ src/main/java/com/matej/luxurycars/services/ReservationServices.java
@@ -5,6 +5,7 @@ import com.matej.luxurycars.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -13,6 +14,7 @@ public interface ReservationServices extends CrudRepository<Reservation,Long> {
     Reservation findById(Long id);
     List<Reservation> findAll();
     List<Reservation> findByUser(User user);
+    @Transactional
     Long deleteById(Long id);
 
 }
--- src/test/java/com/matej/luxurycars/controller/UserControllerTest.java
+++ src/test/java/com/matej/luxurycars/controller/UserControllerTest.java
@@ -47,5 +47,5 @@ public class UserControllerTest {
                 get("/user"))
                 .andExpect(status().isOk());
     }
-    
+
 }
